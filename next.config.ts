06bce3e9d3093cf4b04fec7b23f08320ad1f092e
Puneet Kushwaha <148247654+puneetkushwaha@users.noreply.github.com>
2026-05-04 22:22:46 +0530

    fix: remove outputFileTracingRoot to resolve Vercel double-pathing error

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, "../"),
+  /* outputFileTracingRoot: path.join(__dirname, "../"), */
 };
 
 
